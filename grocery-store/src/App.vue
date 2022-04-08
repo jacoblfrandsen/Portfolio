8e45718bca09b6b4bc9f5c9947c3a3e33a29099d
Jacob <jaocb.l.frandsen@gmail.com>
2022-04-08 17:54:48 -0600

    commint

---
+++ grocery-store/src/App.vue
@@ -0,0 +1,126 @@
+<template>
+<div id="app">
+  <div id="menu">
+    <div id="brand">
+      <router-link to="/">
+        <img src="/images/logo.png">
+      </router-link>
+    </div>
+    <div id="side">
+      <router-link to="/browse">
+        <div class="menu-item browse">
+          <img src="/images/globe.png">
+          <p>Browse</p>
+        </div>
+      </router-link>
+      <router-link to="/cart">
+        <div class="menu-item">
+          <img src="/images/love.png">
+          <p>{{this.$root.$data.cart.length}} Cart</p>
+        </div>
+      </router-link>
+    </div>
+  </div>
+  <router-view />
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numberOfItems: this.$root.$items.length,
+    }
+  },
+  computed: {
+    totalItems() {
+      return this.$root.$items.length;
+    }
+  },
+  methods: {
+    select(country) {
+      this.country = country;
+    },
+    
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 50px 100px;
+
+}
+
+#menu {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 5px;
+  grid-template-areas: "none brand side";
+  margin-bottom: 50px;
+}
+
+#menu a {
+  color: #B84901;
+}
+
+#brand {
+  grid-area: brand;
+  display: flex;
+  justify-content: center;
+}
+
+#brand img {
+  height: 200px;
+}
+
+#side {
+  grid-area: side;
+  display: flex;
+  justify-content: right;
+}
+
+#side img {
+  width: 50px;
+}
+
+.menu-item {
+  display: flex;
+  flex-direction: column;
+}
+
+.menu-item p {
+  margin: 0px;
+}
+
+.browse {
+  margin-right: 50px;
+}
+
+</style>
---
+++ grocery-store/src/views/Cart.vue
@@ -0,0 +1,76 @@
+
+<template>
+<div>
+    <h1 class="empty">Shopping Cart</h1>
+    <div class="empty" v-if="this.$root.$data.cart.length === 0">
+      Your cart is Empty!
+    </div>
+    <div v-else>
+      <CartList :items="items" />
+    </div>
+</div>
+</template>
+
+
+<script>
+import CartList from "../components/CartList.vue"
+export default {
+  name: 'Cart-C',
+  components: {
+    CartList
+  },
+  data() {
+    return {
+      searchText: '',
+    }
+  },
+  computed: {
+    items() {
+      return this.$root.$data.cart;
+    }
+  },
+  methods: {
+    select(){
+      return '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.search {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 50%;
+}
+
+form {
+  display: table;
+  width: 100%;
+}
+
+i {
+  display: table-cell;
+  padding-left: 10px;
+  width: 1px;
+}
+
+input {
+  display: table-cell;
+  font-size: 20px;
+  border: none !important;
+  box-shadow: none !important;
+  width: 100%;
+  height: 40px;
+}
+
+.empty{
+    text-align:center;
+}
+</style>
